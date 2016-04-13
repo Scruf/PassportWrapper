04c67a4c23764f28f05b805ef6d05592fa00d905
Egor <egor.kozitski@mail.citytech.cuny.edu>
2016-04-13 17:27:56 -0400

    Movies api GET method added

---
+++ Movie.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var movieSchema  = new Schema({
+	title:String,
+	path:String,
+	drive_name:String,
+	size:String
+});
+
+var Movie = mongoose.model('Movie',movieSchema);
+module.exports=Movie;
